80113383638bc5a7396975b8f675c8d0688a794f
Julio César Wilches <juliocesar.wil@hotmail.com>
2025-06-20 20:02:38 -0500

    Update login.js
    Botón usuarios locales para prueba sin conexión a Base de Datos.

--- js-empleado/login.js
+++ js-empleado/login.js
@@ -43,6 +43,47 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+    // Botón para restablecer usuarios de prueba
+    btnResetUsuarios.addEventListener("click", () => {
+        const usuariosPrueba = [
+          {
+            nombre: "Carlos Pérez",
+            documento: "123456789",
+            usuario: "carlos",
+            contrasena: "123456789",
+            contrasenaTemporal: true,
+            correo: "juliocesarwil88@gmail.com",
+            rol: "empleado",
+            activo: true,
+            cargo: "Mesero",
+            salario: "$2.500.000",
+            fechaIngreso: "2023-05-10",
+            tipoContrato: "Indefinido",
+            fechaFinContrato: "",
+            diasVacaciones: 30
+          },
+          {
+            nombre: "Laura Gómez",
+            documento: "987654321",
+            usuario: "laura",
+            contrasena: "987654321",
+            contrasenaTemporal: true,
+            correo: "admin@burgueseria.com",
+            rol: "admin",
+            activo: true,
+            cargo: "Administrador",
+            salario: 3500000,
+            fechaIngreso: "2022-01-15",
+            tipoContrato: "Definido",
+            fechaFinContrato: "2025-12-31",
+            diasVacaciones: 30
+          }
+        ];
+      
+        localStorage.setItem("usuarios", JSON.stringify(usuariosPrueba));
+        alert("✅ Usuarios de prueba restablecidos");
+      });
+
   function mostrarError(mensaje) {
     errorMensaje.textContent = mensaje;
     errorMensaje.style.color = "red";
